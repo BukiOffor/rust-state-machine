0b4f79b7751f2c07c30dd72d52c5d2dd3f9629b1
BukiOffor <ebuka2264@yahoo.com>
2024-09-27 23:49:44 +0100

    feat:

--- src/types.rs
+++ src/types.rs
@@ -1,3 +1,15 @@
+use std::ops::AddAssign;
+
+use num::{One, Zero};
+
+
+pub trait Config {
+    type AccountId: Ord;
+    type BlockNumber: Zero + One + AddAssign + Copy;
+    type Nonce: Zero + One + Copy;
+}
+
+
 pub type AccountId = String;
 pub type Balance = u128;
 pub type Nonce = u32;
